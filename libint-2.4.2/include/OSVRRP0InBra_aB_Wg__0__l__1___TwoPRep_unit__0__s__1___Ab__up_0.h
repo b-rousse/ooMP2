ce813fb32662ee37d990fd2bcfbd53175d9ecf2f
Ben Rousseau <b.g.rousseau@gmail.com>
2020-02-21 13:26:49 -0500

    full purge of overlap directories. Git only monitors libint-2.4.2 now

--- libint-2.4.2/include/OSVRRP0InBra_aB_Wg__0__l__1___TwoPRep_unit__0__s__1___Ab__up_0.h
+++
@@ -1,7 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-void OSVRRP0InBra_aB_Wg__0__l__1___TwoPRep_unit__0__s__1___Ab__up_0(const Libint_t* inteval, LIBINT2_REALTYPE* target, const LIBINT2_REALTYPE* src0, const LIBINT2_REALTYPE* src1);
-#ifdef __cplusplus
-};
-#endif
